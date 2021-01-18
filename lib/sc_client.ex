34fe0b23d099bbe5bd6fc8c77553a9d6f8f7ba66
Bill Allen <bill@k2bea.org>
2021-01-17 20:45:05 -0500

    add set_control. working for freq, but not amp ??

--- lib/sc_client.ex
+++ lib/sc_client.ex
@@ -6,37 +6,6 @@ defmodule ScClient do
   encoded_message = OSC.encode("/d_load", "synthdefs.void.scsyndef"])
   """
 
-  # @port 57110
-
-  # def open() do
-  #   :gen_udp.open(@port, [:binary, {:active, false}])
-  # end
-
-
-  # def t(socket) do
-  #   encoded_message = OSC.encode("/quit", [24, 0])
-  #   IO.inspect(encoded_message)
-  #   :gen_udp.send(socket, {192,168,4,39}, @port, encoded_message)
-  #   {:ok, {_ip, _port, msg}} = :gen_udp.recv(socket, 0, 5000)
-  #   IO.inspect(OSC.decode(msg))
-    # Logger.info("ip = #{ip} port = #{port} #{msg}")
-
-    # :gen_udp.send(socket, {192,168,4,39}, 57110,
-    #   OSC.encode("/n_set", [1001, "defpath", "/home/bill/src/sc/examples/synthdefs/sine.scsyndef"]))
-    # {:ok, {ip, port, msg}} = :gen_udp.recv(socket, 0)
-    # Logger.info("ip = #{ip} port = #{port} #{msg}")
-
-    # :gen_udp.send(socket, {192,168,4,39}, 57110,
-    #   OSC.encode("/d_load", ["/home/bill/src/sc/examples/synthdefs/sine.scsyndef"]))
-    # {:ok, {ip, port, msg}} = :gen_udp.recv(socket, 0)
-    # Logger.info("ip = #{ip} port = #{port} #{msg}")
-
-    # :gen_udp.send(socket, {192,168,4,39}, 57110,
-    #   OSC.encode("/s_new", ["sine", 1001, 1, 0]))
-    # {:ok, {ip, port, msg}} = :gen_udp.recv(socket, 0)
-    # Logger.info("ip = #{ip} port = #{port} #{msg}")
-# end
-
   def status() do
     GenServer.call(ScEm, :status)
   end
@@ -49,14 +18,14 @@ defmodule ScClient do
     sendMsg({"/d_loadDir", [dir]})
   end
 
-  @doc """
-  This depends on having a SynthDef named quick1 previously defined and stored
-  in supercollider.
-  """
   def make_sound(synth) do
     id = GenServer.call(ScEm, :next_id)
-    sendMsg({"/s_new", [synth, id, 1, 0]})
+    sendMsg({"/s_new", [synth, id, 0, 1]})
     id
+  end
+
+  def set_control(id, control, val) do
+    sendMsg({"/n_set", [id, control, val]})
   end
 
   def stop_sound(id) do
