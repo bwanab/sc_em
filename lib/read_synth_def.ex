579e97dd3fbdc5e9c5a53437f01f44d27d8e6c1e
Bill Allen <bill@k2bea.org>
2021-01-21 11:01:24 -0500

    make a generic array function that all arrays can use since all follow same pattern

--- lib/read_synth_def.ex
+++ lib/read_synth_def.ex
@@ -10,7 +10,7 @@ defmodule ReadSynthDef do
     ftype = String.slice(f, 0..3)
     {fversion, n1} = int32(f, 4)
     {ndefs, n2} = int16(f, n1)
-    {synth_defs, _} = vals_and_index_from_array(synth_defs(ndefs, f, n2))
+    {synth_defs, _} = get_array(ndefs, f, n2, &synth_def_val/2)
     %{:ftype => ftype,
       :fversion => fversion,
       :ndefs => ndefs,
@@ -18,26 +18,24 @@ defmodule ReadSynthDef do
     }
   end
 
-  def synth_defs(ndefs, _f, index) when ndefs == 0 do [index] end
-
-  def synth_defs(ndefs, f, index) do
+  def synth_def_val(f, index) do
     Logger.debug("index = #{index}")
     {name, n1} = pstring(f, index)
     Logger.debug("name = #{name} n1 = #{n1}")
     {n_constants, n2} = int32(f, n1)
     Logger.debug("n_constants = #{n_constants}")
-    {const_vals, n3} = vals_and_index_from_array(float_array(n_constants, f, n2))
+    {const_vals, n3} = get_array(n_constants, f, n2, &float32/2)
     {n_parameters, n4} = int32(f, n3)
-    {parameter_vals, n5} = vals_and_index_from_array(float_array(n_parameters, f, n4))
+    {parameter_vals, n5} = get_array(n_parameters, f, n4, &float32/2)
     {n_parameter_names, n6} = int32(f, n5)
-    {parameter_names, n7} = vals_and_index_from_array(param_array(n_parameter_names, f, n6))
+    {parameter_names, n7} = get_array(n_parameter_names, f, n6, &parameter_name_val/2)
     {n_ugens, n8} = int32(f, n7)
-    {ugens, n9} = vals_and_index_from_array(ugen_array(n_ugens, f, n8))
+    {ugens, n9} = get_array(n_ugens, f, n8, &ugen_val/2)
     {n_variants, n10} = int16(f, n9)
-    {variants, _} = vals_and_index_from_array(variant_array(n_variants, f, n10))
+    {variants, _} = get_array(n_variants, f, n10, &variant_val/2)
 
     # the last val is the index.
-    [%{:name => name,
+    {%{:name => name,
        :n_constants => n_constants,
        :const_vals => const_vals,
        :n_parameters => n_parameters,
@@ -47,21 +45,20 @@ defmodule ReadSynthDef do
        :ugens => ugens,
        :n_variants => n_variants,
        :variants => variants
-      }]
-    ++ synth_defs(ndefs - 1, f, n3)
+      }, n3}
   end
 
-  def ugen_array(n, _f, index) when n == 0 do [index] end
 
-  def ugen_array(n, f, index) do
+  def ugen_val(f, index) do
     {ugen_name, n1} = pstring(f, index)
     {calc_rate, n2} = int8(f, n1)
     {n_inputs, n3} = int32(f, n2)
     {n_outputs, n4} = int32(f, n3)
     {special_index, n5} = int16(f, n4)
-    {inputs, n6} = vals_and_index_from_array(input_array(n_inputs, f, n5))
-    {outputs, n7} = vals_and_index_from_array(output_array(n_outputs, f, n6))
-    [%{
+    {inputs, n6} = get_array(n_inputs, f, n5, &input_val/2)
+    {outputs, n7} = get_array(n_outputs, f, n6, &int8/2)
+    {
+      %{
         :ugen_name => ugen_name,
         :calc_rate => calc_rate,
         :n_inputs => n_inputs,
@@ -69,50 +66,52 @@ defmodule ReadSynthDef do
         :special_index => special_index,
         :inputs => inputs,
         :outputs => outputs
-     }
-    ] ++ ugen_array(n-1, f, n7)
+     }, n7}
   end
 
-  def input_array(n, _f, index) when n == 0 do [index] end
 
-  def input_array(n, f, index) do
+  def input_val(f, index) do
     {ugen_index, n1} = int32(f, index)
     {other_index, n2} = int32(f, n1)
-    [%{:ugen_index => ugen_index,
-       :other_index => other_index}] ++ input_array(n-1, f, n2)
+    {%{:ugen_index => ugen_index,
+       :other_index => other_index}, n2}
   end
 
-  def output_array(n, _f, index) when n == 0 do [index] end
-
-  def output_array(n, f, index) do
-    {calc_rate, n1} = int8(f, index)
-    [calc_rate] ++ output_array(n-1, f, n1)
-  end
-
-
-  def float_array(n, _f, index) when n == 0 do [index] end
-
-  def float_array(n, f, index) do
-    {val, n1} = float32(f, index)
-    [val] ++ float_array(n-1, f, n1)
-  end
-
-  def param_array(n, _f, index) when n == 0 do [index] end
-
-  def param_array(n, f, index) do
+  def parameter_name_val(f, index) do
     {val, n1} = pstring(f, index)
-    {index, n2} = int32(f, n1)
-    [{val, index}] ++ param_array(n-1, f, n2)
+    {val_index, n2} = int32(f, n1)
+    {{val, val_index}, n2}
   end
 
-  def variant_array(n, _f, index) when n == 0 do [index] end
-
-  def variant_array(n, f, index) do
+  def variant_val(f, index) do
     {name, n1} = pstring(f, index)
     {parameter, n2} = int32(f, n1)
-    [{name, parameter}] ++ variant_array(n-1, f, n2)
+    {{name, parameter}, n2}
   end
 
+  ##
+  ## Generic array getter
+  ##
+
+  def get_array_help(n, _data, index, _fun) when n == 0 do [index] end
+
+  def get_array_help(n, data, index, fun) do
+    {vals, new_index} = fun.(data, index)
+    [vals] ++ get_array_help(n-1, data, new_index, fun)
+  end
+
+
+  def get_array(n, data, index, fun) do
+    vals_and_index_from_array(get_array_help(n, data, index, fun))
+  end
+
+  def vals_and_index_from_array(a) do
+    {Enum.take(a, length(a) - 1), List.last(a)}
+  end
+
+  ##
+  ##  conversion primatives
+  ##
   def int32(s, i) do
     <<res :: signed-big-integer-32>> = String.slice(s, i..i+3)
     {res, i + 4}
@@ -139,8 +138,5 @@ defmodule ReadSynthDef do
     {String.slice(s, i+1..i+size), size + n}
   end
 
-  def vals_and_index_from_array(a) do
-    {Enum.take(a, length(a) - 1), List.last(a)}
-  end
 
   end
