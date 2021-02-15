4dca7452126d48421a25a3016d3b25573632e1ac
Bill Allen <bill@k2bea.org>
2021-02-15 12:57:05 -0500

    get a random note walker actually playing

--- lib/modsynth_rand.ex
+++ lib/modsynth_rand.ex
@@ -1,9 +1,11 @@
 defmodule Modsynth.Rand.State do
   defstruct last_note: 0,
     last_rhythm: 0,
+    note_control: 0,
     scale: []
   @type t :: %__MODULE__{last_note: integer,
                          last_rhythm: integer,
+                         note_control: integer,
                          scale: list
   }
 end
@@ -40,6 +42,25 @@ defmodule Modsynth.Rand do
     GenServer.call(pid, :next)
   end
 
+  def play(pid, _file) do
+    controls = Modsynth.play("examples/cc-in3.json")
+    {_, note, _} = Enum.find(controls, fn {name, _, _} -> name == "cc-in_to_note-freq" end)
+    # {_, amp, _} = Enum.find(controls, fn {name, _, _} -> name == "cc-in_to_amp" end)
+    {_, splitter_level, _} = Enum.find(controls, fn {name, _, _} -> name == "const_to_a-splitter" end)
+    ScClient.set_control(splitter_level, "in", 1)
+    Logger.info("note control: #{note}")
+    GenServer.call(pid, {:set_note_control, note})
+    {first_note, first_dur} = next(pid)
+    Logger.info("first note #{first_note} first dur #{first_dur}")
+    ScClient.set_control(note, "in", first_note)
+    schedule_next_note(pid, first_dur)
+  end
+
+
+  defp schedule_next_note(pid, dur) do
+    Logger.info("schedule next note at: #{dur * 250}")
+    Process.send_after(pid, :next_note, dur * 250)
+  end
 
   #######################
   # implementation
@@ -62,12 +83,29 @@ defmodule Modsynth.Rand do
   end
 
   @impl true
+  def handle_call({:set_note_control, control}, _from, state) do
+    {:reply, :ok, %State{state | note_control: control}}
+  end
+
+  @impl true
   def handle_call(:next, _from, %State{last_note: last_note,
                                        scale: scale} = state) do
     next_note = Modsynth.Rand.rand(last_note, scale)
     next_rhythm = rhythm()
     {:reply, {next_note, next_rhythm}, %State{state | last_note: next_note, last_rhythm: next_rhythm}}
   end
+
+  @impl true
+  def handle_info(:next_note, %State{last_note: last_note,
+                                     scale: scale,
+                                     note_control: note} = state) do
+    next_note = Modsynth.Rand.rand(last_note, scale)
+    next_rhythm = rhythm()
+    ScClient.set_control(note, "in", next_note)
+    schedule_next_note(self(), next_rhythm)
+    {:noreply, %State{state | last_note: next_note, last_rhythm: next_rhythm}}
+  end
+
   
   def closest(note, scale) do
     index = Enum.find_index(scale, fn x -> x > note end)
